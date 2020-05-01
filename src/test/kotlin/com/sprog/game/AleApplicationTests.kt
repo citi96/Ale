7ee7cebaaaea7840504882449d4e194c95125d88
citi <fchiti071@gmail.com>
2020-05-01 18:29:13 +0200

    First commit

---
+++ src/test/kotlin/com/sprog/game/AleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sprog.game
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
